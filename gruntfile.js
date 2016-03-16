a88bab078bcab4c00a6ec3a9d1c0574bfa8c0f6c
dignifiedquire <dignifiedquire@gmail.com>
2016-03-16 22:22:58 +0100

    chore(build): Fix release task

--- gruntfile.js
+++ gruntfile.js
@@ -25,6 +25,29 @@ module.exports = function (grunt) {
         commitMessage: 'chore: update contributors'
       }
     },
+    conventionalChangelog: {
+      release: {
+        options: {
+          changelogOpts: {
+            preset: 'angular'
+          }
+        },
+        src: 'CHANGELOG.md'
+      }
+    },
+    conventionalGithubReleaser: {
+      release: {
+        options: {
+          auth: {
+            type: 'oauth',
+            token: process.env.GH_TOKEN
+          },
+          changelogOpts: {
+            preset: 'angular'
+          }
+        }
+      }
+    },
     bump: {
       options: {
         commitMessage: 'chore: release v%VERSION%',
@@ -54,9 +77,10 @@ module.exports = function (grunt) {
   grunt.registerTask('release', 'Bump the version and publish to NPM.', function (type) {
     grunt.task.run([
       'npm-contributors',
-      'bump-only:' + (type || 'patch'),
-      'changelog',
+      'bump:' + (type || 'patch') + ':bump-only',
+      'conventionalChangelog',
       'bump-commit',
+      'conventionalGithubReleaser',
       'npm-publish'
     ])
   })
