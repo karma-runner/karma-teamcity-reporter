42c9b3fb18af535d9ac0584198068dee614eca48
fc-franz <Franziskus1988@users.noreply.github.com>
2016-11-03 16:21:02 +0100

    Fix unittests

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -18,23 +18,23 @@ describe('TeamCity reporter', function () {
   it('should produce 2 standard messages without browsers', function () {
     reporter.onRunStart([])
     reporter.onRunComplete([])
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests']\n")
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests' flowId='']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests' flowId='']\n")
   })
 
   it('should produce 2 standard messages without tests', function () {
     reporter.onRunStart([mosaic])
     reporter.onRunComplete([])
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests']\n")
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests' flowId='']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests' flowId='']\n")
   })
 
   it('should produce messages with one test', function () {
     reporter.onRunStart([mosaic])
     reporter.specSuccess(mosaic, {description: 'SampleTest', time: 2, suite: ['Suite 1']})
     reporter.onRunComplete([])
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests']\n")
-    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockOpened name='JavaScript Unit Tests' flowId='']\n")
+    expect(reporter.write).to.have.been.calledWith("##teamcity[blockClosed name='JavaScript Unit Tests' flowId='']\n")
     expect(reporter.write).to.have.been.calledWith(
       "##teamcity[testSuiteStarted name='Suite 1.Mosaic']\n"
     )
